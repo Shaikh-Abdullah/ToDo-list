d5c32adc796011dde6a55b0d6802bb6c767178da
Abdullah <shabdullah685@gmail.com>
2022-07-01 21:48:42 +0530

    Some Changes

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -53,7 +53,7 @@ const CourseInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
-        <label>Course Goal</label>
+        <label>What ToDo Next?</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
